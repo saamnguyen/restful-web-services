ba367f006dace992c9c60b35e67a848ae6650174
samukma <nguyensam576@gmail.com>
2021-11-19 23:56:36 +0700

    exception

---
+++ src/main/java/com/spring/restfulwebservices/exception/ExceptionResponse.java
@@ -0,0 +1,8 @@
+package com.spring.restfulwebservices.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+    public ExceptionResponse(Date date, String message, String description) {
+    }
+}
